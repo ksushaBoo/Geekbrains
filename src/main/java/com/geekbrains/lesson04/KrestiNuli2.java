2d6359ffae57ebb1a005205d56a6b346f6a42305
Ksenia Zakharenkova <ksenia.zakharenkova@russianpost.ru>
2022-01-19 18:55:12 +0300

    lesson4 homework

--- src/main/java/com/geekbrains/lesson04/KrestiNuli2.java
+++ src/main/java/com/geekbrains/lesson04/KrestiNuli2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class KrestiNuli2 {
 
-    private final static int SIZE = 3;
+    private final static int SIZE = 4;
     private final static char DOT_EMPTY = '.';
     private final static char DOT_X = 'X';
     private final static char DOT_O = 'O';
@@ -51,23 +51,26 @@ public class KrestiNuli2 {
     private static boolean checkWin(char symbol) {
 
         char[] resultLine = new char[SIZE];
+        char[] resultColumn = new char[SIZE];
         char[] resultMainDiagonal = new char[SIZE];
         char[] resultSecondDiagonal = new char[SIZE];
         char[] winAResult = new char[SIZE];
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                resultLine[j] = MAP[i][j];
-                winAResult[j] = symbol;
+                resultLine[j] = MAP[i][j]; // Складываем все  символы в текущей линии в отдельный массив
+                resultColumn[j] = MAP[j][i]; // Складываем все  символы в текущем стобце в отдельный массив
+                winAResult[j] = symbol; // Заполняем массив символами для сверки победы массив Х или О
                 if (i + j == SIZE - 1) {
-                    resultSecondDiagonal[j] = MAP[i][j];
+                    resultSecondDiagonal[j] = MAP[i][j]; // Складываем все  символы в  побочной диагонали в отдельный массив
                 }
                 if (i == j) {
-                    resultMainDiagonal[j] = MAP[i][j];
+                    resultMainDiagonal[j] = MAP[i][j]; // Складываем все  символы в  главной диагонали в отдельный массив
                 }
             }
-            if (Arrays.equals(resultLine, winAResult) || Arrays.equals(resultMainDiagonal, winAResult) || Arrays.equals(resultSecondDiagonal, winAResult))
-                return true;
+            if (Arrays.equals(resultLine, winAResult) || (Arrays.equals(resultColumn, winAResult))
+                    || Arrays.equals(resultMainDiagonal, winAResult) || Arrays.equals(resultSecondDiagonal, winAResult))
+                return true; // Сравниваем все возможные победные коомбинации с массивом выгрышных символоы
         }
         return false;
     }
@@ -83,65 +86,60 @@ public class KrestiNuli2 {
     }
 
     private static void aiTurn() {
-        int x;
-        int y;
+        int x = 0;
+        int y = 0;
+        boolean aiWin = false;
+        boolean userWin = false;
 
-        do {
+        //Проверяем возможен ли победный ход для ИИ: проходим по всему полю, в валидные  ячейки ставим О
+        // и делаем проверку победы, Если победа, то сохряняем х и у
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (isCellValid(j, i)) {
+                    MAP[i][j] = DOT_O;
+                    if (checkWin(DOT_O)) {
+                        x = j;
+                        y = i;
+                        aiWin = true;
+                    }
+                    MAP[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+
+//        Проверяем возможность заблокировать игрока. Проходим по всем валидным ячейкам и заполняем их Х, проверяем победу.
+//        Если победа состоялась, то запоминаем х и у
+        if (!aiWin) {
+            for (int i = 0; i < SIZE; i++)
+                for (int j = 0; j < SIZE; j++) {
+                    if (isCellValid(j, i)) {
+                        MAP[i][j] = DOT_X;
+                        if (checkWin(DOT_X)) {
+                            x = j;
+                            y = i;
+                            userWin = true;
+                        }
+                        MAP[i][j] = DOT_EMPTY;
+                    }
+                }
+        }
+
+        if (!aiWin && !userWin) {
             do {
                 x = RANDOM.nextInt(SIZE);
                 y = RANDOM.nextInt(SIZE);
-            } while (!blockHumanTurn(x, y));
+            }
+            while (!isCellValid(x, y));
         }
-        while (!isCellValid(x, y));
 
         MAP[y][x] = DOT_O;
     }
 
-    private static boolean blockHumanTurn(int x, int y) {
-
-        int countLine = 0;
-        int countMain = 0;
-        int countSecond = 0;
-
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if ((i + j == SIZE - 1) && (MAP[i][j] == DOT_X) && (x + y == i + j)) {
-                    countSecond++;
-                    if (countSecond > 1)
-                    return true;
-                }
-                if ((i == j) && (MAP[i][j] == DOT_X) && (x == y)) {
-                    countMain++;
-                    if (countMain > 1)
-                        return true;
-                }
-                if ((MAP[i][j] == DOT_X) && (y == i)) {
-                    countLine++;
-                    if (countLine > 1)
-                        return true;
-                }
-            }
-            countLine = 0;
-        }
-        return false;
-    }
-
-//    private static void aiTurn() {
-//        int x;
-//        int y;
-//
-//        do {
-//            x = RANDOM.nextInt(SIZE);
-//            y = RANDOM.nextInt(SIZE);
-//        }
-//        while (!isCellValid(x, y));
-//
-//        MAP[y][x] = DOT_O;
-//    }
 
     private static void humanTurn() {
         int x;
         int y;
+        int count = 0;
 
         do {
             System.out.println("Введите координаты в формате Х и У");
@@ -149,7 +147,9 @@ public class KrestiNuli2 {
             y = SCANNER.nextInt() - 1;
         } while (!isCellValid(x, y));
 
+        count++;
         MAP[y][x] = DOT_X;
+        System.out.println("Ход номер " + count);
     }
 
     private static boolean isCellValid(int x, int y) {
